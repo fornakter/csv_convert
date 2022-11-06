e9870fc0e05207b35aa5b367734b4fec5ff63314
AdamF <fornakter@gmail.com>
2022-11-06 15:19:27 +0100

    Fix some bugs

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import csv
 import pandas as pd
 
+text = []
+with open('product.csv') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        text.append(row)
+title = []
+for i in range(12):
+    title.append(text[i])
+    df = pd.DataFrame(title)
+    df.to_markdown(index=False)
+    df.to_csv("product1.csv", sep=';')
 
-def open():
-    text = []
-    with open('product.csv') as csvfile:
-        spamreader = csv.reader(csvfile)
-        for row in spamreader:
-            text.append(row)
-    title = []
-    for i in range(12):
-        title.append(text[i])
-        df = pd.DataFrame(title)
-        df.to_csv("product1.csv", sep=';')
 
-
-if __name__ == '__main__':
-    open()
